76c7fcdcb7c530b74843648672aa3c8df70938b6
TaxiarchisVaxevanis <taxvax05@gmail.com>
2026-05-11 21:18:51 +0300

    "Fixed access to the db"

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql2/promise');
 
-// Απευθείας σύνδεση με τις τοπικές ρυθμίσεις της MySQL
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',         // Το προεπιλεγμένο username
-    password: 'taxvax2005',         // Κενό password για το XAMPP
-    database: 'unibite_db', // Το όνομα της βάσης που δημιούργησες
-    port: 3306            // Η τυπική θύρα της MySQL
+    user: 'root',        
+    password: '',      
+    database: 'unibite_db', 
+    port: 3306            
 });
 
 module.exports = pool;
